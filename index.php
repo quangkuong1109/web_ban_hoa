d81ebdb46bf10583b5ed045fd95bddb4be40b375
MinThang03 <minhthang031123@gmail.com>
2024-10-21 17:15:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -583,7 +583,7 @@
         <div class="row border-top mx-xl-5 py-4" style="border-color: rgba(256, 256, 256, .1) !important;">
             <div class="col-md-6 px-xl-0">
                 <p class="mb-md-0 text-center text-md-left text-secondary">
-                    &copy; <a class="text-primary" href="#">CÔNG TY TNHH CƯỜNG-DUY-THẮNG</a>. All Rights Reserved. <br>Designed by <br> Phí Đình Duy | Nguyễn Quang Cường | Nguyễn Minh Thắng
+                    &copy; <a class="text-primary" href="#">CÔNG TY TNHH CƯỜNG-DUY-THẮNG</a>. All Rights Reserved. <br>Designed by <br> Phí Đình Duy | Nguyễn Quang Cường | Phạm Minh Thắng
                     <a class="text-primary" href="https://htmlcodex.com">HTML Codex</a> -->
                 <!-- </p>
             </div>
