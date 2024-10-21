8e74df7ad12a852be4b9d0ef01839f6455dbc357
quangkuong1109 <cuong8anc@gmail.com>
2024-10-21 17:42:24 +0700

    database

---
+++ banhoa .sql
@@ -0,0 +1,329 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 21, 2024 at 12:41 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `banhoa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chitietdonhang`
+--
+
+CREATE TABLE `chitietdonhang` (
+  `MaChiTietDonHang` int(11) NOT NULL,
+  `MaDonHang` int(11) NOT NULL,
+  `MaSanPham` int(11) NOT NULL,
+  `SoLuong` int(11) NOT NULL,
+  `GIa` decimal(10,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `danhgia`
+--
+
+CREATE TABLE `danhgia` (
+  `MaDanhGia` int(11) NOT NULL,
+  `MaSanPham` int(11) NOT NULL,
+  `MaKhachHang` int(11) NOT NULL,
+  `DiemDanhGia` int(11) NOT NULL,
+  `BinhLuan` text NOT NULL,
+  `NgayDanhGia` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `danhmucsanpham`
+--
+
+CREATE TABLE `danhmucsanpham` (
+  `MaDanhMuc` int(11) NOT NULL,
+  `TenDanhMuc` varchar(200) NOT NULL,
+  `MoTa` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `donhang`
+--
+
+CREATE TABLE `donhang` (
+  `MaDonHang` int(11) NOT NULL,
+  `MaKhachHang` int(11) NOT NULL,
+  `NgayDatHang` datetime NOT NULL,
+  `TongGiaTri` decimal(10,0) NOT NULL,
+  `TrangThai` varchar(50) NOT NULL,
+  `MaGiamGia` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `giaohang`
+--
+
+CREATE TABLE `giaohang` (
+  `MaGiaoHang` int(11) NOT NULL,
+  `MaDonHang` int(11) NOT NULL,
+  `NgayGiaoHangDuKien` datetime NOT NULL,
+  `DiaChiGiaoHang` text NOT NULL,
+  `TrangThaiGiaoHang` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `khachhang`
+--
+
+CREATE TABLE `khachhang` (
+  `MaKhachHang` int(11) NOT NULL,
+  `TenTaiKhoan` varchar(50) NOT NULL,
+  `TenKhachHang` varchar(200) NOT NULL,
+  `Email` varchar(200) NOT NULL,
+  `SoDienThoai` varchar(15) NOT NULL,
+  `DiaChi` text NOT NULL,
+  `MatKhau` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quantrivien`
+--
+
+CREATE TABLE `quantrivien` (
+  `MaQuanTriVien` int(11) NOT NULL,
+  `TenDangNhap` varchar(50) NOT NULL,
+  `MatKhau` varchar(200) NOT NULL,
+  `VaiTro` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sanpham`
+--
+
+CREATE TABLE `sanpham` (
+  `MaSanPham` int(11) NOT NULL,
+  `TenSanPham` varchar(200) NOT NULL,
+  `MaDanhMuc` int(11) NOT NULL,
+  `MoTa` text NOT NULL,
+  `Gia` decimal(10,0) NOT NULL,
+  `HinhAnh` varchar(200) NOT NULL,
+  `TonKho` int(11) NOT NULL,
+  `GiamGia` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `thanhtoan`
+--
+
+CREATE TABLE `thanhtoan` (
+  `MaThanhToan` int(11) NOT NULL,
+  `MaDonHang` int(11) NOT NULL,
+  `PhuongThucThanhToan` int(11) NOT NULL,
+  `NgayThanhToan` int(11) NOT NULL,
+  `SoTien` decimal(10,0) NOT NULL,
+  `TrangThai` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chitietdonhang`
+--
+ALTER TABLE `chitietdonhang`
+  ADD PRIMARY KEY (`MaChiTietDonHang`),
+  ADD KEY `fk_chitietdonhang_donhang` (`MaDonHang`),
+  ADD KEY `fk_chitietdonhang_sanpham` (`MaSanPham`);
+
+--
+-- Indexes for table `danhgia`
+--
+ALTER TABLE `danhgia`
+  ADD PRIMARY KEY (`MaDanhGia`),
+  ADD KEY `fk_danhgia_makhachhang` (`MaKhachHang`),
+  ADD KEY `fk_danhgia_masanpham` (`MaSanPham`);
+
+--
+-- Indexes for table `danhmucsanpham`
+--
+ALTER TABLE `danhmucsanpham`
+  ADD PRIMARY KEY (`MaDanhMuc`);
+
+--
+-- Indexes for table `donhang`
+--
+ALTER TABLE `donhang`
+  ADD PRIMARY KEY (`MaDonHang`),
+  ADD KEY `MaKhachHang` (`MaKhachHang`);
+
+--
+-- Indexes for table `giaohang`
+--
+ALTER TABLE `giaohang`
+  ADD PRIMARY KEY (`MaGiaoHang`),
+  ADD KEY `fk_giaohang_madonhang` (`MaDonHang`);
+
+--
+-- Indexes for table `khachhang`
+--
+ALTER TABLE `khachhang`
+  ADD PRIMARY KEY (`MaKhachHang`),
+  ADD UNIQUE KEY `TenTaiKhoan` (`TenTaiKhoan`),
+  ADD UNIQUE KEY `Email` (`Email`);
+
+--
+-- Indexes for table `quantrivien`
+--
+ALTER TABLE `quantrivien`
+  ADD PRIMARY KEY (`MaQuanTriVien`),
+  ADD UNIQUE KEY `TenDangNhap` (`TenDangNhap`);
+
+--
+-- Indexes for table `sanpham`
+--
+ALTER TABLE `sanpham`
+  ADD PRIMARY KEY (`MaSanPham`),
+  ADD KEY `MaDanhMuc` (`MaDanhMuc`);
+
+--
+-- Indexes for table `thanhtoan`
+--
+ALTER TABLE `thanhtoan`
+  ADD PRIMARY KEY (`MaThanhToan`),
+  ADD KEY `fk_thanhtoan_madonhang` (`MaDonHang`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chitietdonhang`
+--
+ALTER TABLE `chitietdonhang`
+  MODIFY `MaChiTietDonHang` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `danhgia`
+--
+ALTER TABLE `danhgia`
+  MODIFY `MaDanhGia` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `danhmucsanpham`
+--
+ALTER TABLE `danhmucsanpham`
+  MODIFY `MaDanhMuc` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `donhang`
+--
+ALTER TABLE `donhang`
+  MODIFY `MaDonHang` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `giaohang`
+--
+ALTER TABLE `giaohang`
+  MODIFY `MaGiaoHang` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `khachhang`
+--
+ALTER TABLE `khachhang`
+  MODIFY `MaKhachHang` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `quantrivien`
+--
+ALTER TABLE `quantrivien`
+  MODIFY `MaQuanTriVien` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sanpham`
+--
+ALTER TABLE `sanpham`
+  MODIFY `MaSanPham` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `thanhtoan`
+--
+ALTER TABLE `thanhtoan`
+  MODIFY `MaThanhToan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `chitietdonhang`
+--
+ALTER TABLE `chitietdonhang`
+  ADD CONSTRAINT `MaDonHang` FOREIGN KEY (`MaDonHang`) REFERENCES `donhang` (`MaDonHang`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `MaSanPham` FOREIGN KEY (`MaSanPham`) REFERENCES `sanpham` (`MaSanPham`),
+  ADD CONSTRAINT `fk_chitietdonhang_donhang` FOREIGN KEY (`MaDonHang`) REFERENCES `donhang` (`MaDonHang`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_chitietdonhang_sanpham` FOREIGN KEY (`MaSanPham`) REFERENCES `sanpham` (`MaSanPham`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `danhgia`
+--
+ALTER TABLE `danhgia`
+  ADD CONSTRAINT `fk_danhgia_makhachhang` FOREIGN KEY (`MaKhachHang`) REFERENCES `khachhang` (`MaKhachHang`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_danhgia_masanpham` FOREIGN KEY (`MaSanPham`) REFERENCES `sanpham` (`MaSanPham`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `donhang`
+--
+ALTER TABLE `donhang`
+  ADD CONSTRAINT `MaKhachHang` FOREIGN KEY (`MaKhachHang`) REFERENCES `khachhang` (`MaKhachHang`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `giaohang`
+--
+ALTER TABLE `giaohang`
+  ADD CONSTRAINT `fk_giaohang_madonhang` FOREIGN KEY (`MaDonHang`) REFERENCES `donhang` (`MaDonHang`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `sanpham`
+--
+ALTER TABLE `sanpham`
+  ADD CONSTRAINT `MaDanhMuc` FOREIGN KEY (`MaDanhMuc`) REFERENCES `danhmucsanpham` (`MaDanhMuc`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `thanhtoan`
+--
+ALTER TABLE `thanhtoan`
+  ADD CONSTRAINT `fk_thanhtoan_madonhang` FOREIGN KEY (`MaDonHang`) REFERENCES `donhang` (`MaDonHang`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
