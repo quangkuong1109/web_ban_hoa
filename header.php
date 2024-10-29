332a753b3a4ce9a0be883724c0b87e0feddb6fd8
The21-meatversion <highlawview.pd@gmail.com>
2024-10-29 17:07:44 +0700

    Update header.php

--- header.php
+++ header.php
@@ -188,8 +188,6 @@
         <div class="row align-items-center bg-light py-3 px-xl-5 d-none d-lg-flex">
             <div class="col-lg-4">
                 <a href="index.php" class="text-decoration-none">
-                    <!-- <span class="h1 text-uppercase text-primary bg-dark px-2">CDT</span>
-                <span class="h1 text-uppercase text-dark bg-primary px-2 ml-n1">Flower</span> -->
                     <img width="300px" height="60px" src="img/icon_logo.png">
                 </a>
             </div>
@@ -238,8 +236,7 @@
             <div class="col-lg-9">
                 <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 py-lg-0 px-0">
                     <a href="" class="text-decoration-none d-block d-lg-none">
-                        <span class="h1 text-uppercase text-dark bg-light px-2">CDT</span>
-                        <span class="h1 text-uppercase text-light bg-primary px-2 ml-n1">Flower</span>
+                        <img width="300px" height="60px" src="img/icon_logo.png">
                     </a>
                     <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                         <span class="navbar-toggler-icon"></span>
